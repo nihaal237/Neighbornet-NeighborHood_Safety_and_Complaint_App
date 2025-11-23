7fbceae861bd05360ea199675ca28a4ba41c9468
Nihaal <nihaaldurrani@gmail.com>
2025-11-23 19:31:09 +0500

    Created Backend File for User's Reports

---
+++ neighbornet_backend/myapp/api_views/user_view_reports.py
@@ -0,0 +1,17 @@
+# myapp/views_reports.py
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .serializers_report import ReportSerializer
+from .models import Report
+
+class UserReportsView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        # Get all reports submitted by this authenticated user
+        reports = Report.objects.filter(user=request.user).order_by('-dateTime')
+
+        serializer = ReportSerializer(reports, many=True, context={'request': request})
+
+        return Response(serializer.data)
